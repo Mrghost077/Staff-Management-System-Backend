a6de85bd5137506c7e4624d6720d2cbfefd41cd8
mrghost077 <msnlms86@gmail.com>
2026-01-16 20:58:52 +0530

    fixed the bug

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -184,11 +184,21 @@ export const login = async (req, res) => {
         res.cookie ('token', token, {
             httpOnly : true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? "none" : "strict",
+            sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
-        return res.json({success: true, role: user.role});
+       return res.json({
+        success: true, 
+        role: user.role, 
+        user: {
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            role: user.role,
+            isAccountVerified: user.isAccountVerified
+        }
+    });
 
     } catch(error) {
         return res.json ({success: false, message: error.message});
