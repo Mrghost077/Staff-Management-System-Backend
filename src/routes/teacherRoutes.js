96df005848c04fca6b2efe7313fce427b1b4193b
mrghost077 <msnlms86@gmail.com>
2026-03-01 14:29:03 +0530

    fixed a bug in avatar update

--- src/routes/teacherRoutes.js
+++ src/routes/teacherRoutes.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { getTeacherSettings, updateTeacherSettings, resetTeacherPassword, updateAvatar,} from "../controllers/teacherSettingsController.js";
 import userAuth from "../middleware/userAuth.js";
-import upload from "../middleware/multer.js";
 
 const teacherSettingsRouter = express.Router();
 
 teacherSettingsRouter.get("/profile", userAuth, getTeacherSettings);
 teacherSettingsRouter.patch("/update-profile", userAuth, updateTeacherSettings);
 teacherSettingsRouter.post("/reset-password", userAuth, resetTeacherPassword);
-teacherSettingsRouter.post("/update-avatar", userAuth, upload.single('image'), updateAvatar);
+teacherSettingsRouter.post("/update-avatar", userAuth,  updateAvatar);
 
 export default teacherSettingsRouter;
